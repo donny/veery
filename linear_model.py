ac5ce36620d496fa63790a5b66d17dbc975f476a
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-06-01 08:23:43 +1000

    Add MNIST Softmax

--- linear_model.py
+++ linear_model.py
@@ -11,15 +11,15 @@ linear_model = W * x + b
 y = tf.placeholder(tf.float32)
 
 # Loss function
-loss = tf.reduce_sum(tf.square(linear_model - y)) # sum of the squares
+loss = tf.reduce_sum(tf.square(linear_model - y))  # Sum of the squares
 
 # Optimizer
 optimizer = tf.train.GradientDescentOptimizer(0.01)
 train = optimizer.minimize(loss)
 
 # Training data
-x_train = [1,2,3,4]
-y_train = [0,-1,-2,-3]
+x_train = [1, 2, 3, 4]
+y_train = [0, -1, -2, -3]
 
 # Training loop
 init = tf.global_variables_initializer()
@@ -27,8 +27,9 @@ sess = tf.Session()
 sess.run(init)
 
 for i in range(1000):
-      sess.run(train, {x:x_train, y:y_train})
+    sess.run(train, {x: x_train, y: y_train})
 
-      # Evaluate training accuracy
-      curr_W, curr_b, curr_loss  = sess.run([W, b, loss], {x:x_train, y:y_train})
-      print("W: %s b: %s loss: %s"%(curr_W, curr_b, curr_loss))
+    # Evaluate training accuracy
+    curr_W, curr_b, curr_loss = sess.run(
+        [W, b, loss], {x: x_train, y: y_train})
+    print("W: %s b: %s loss: %s" % (curr_W, curr_b, curr_loss))
